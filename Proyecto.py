2a0f075f5f9bce93b1468ebd31b6cc361b452c91
DilanB <dbvlml@gmail.com>
2025-08-05 02:08:05 -0400

    Eliminacion de errores

--- Proyecto.py
+++ Proyecto.py
@@ -21,8 +21,4 @@ print(ventas_por_genero)
 ventas_por_anio = df.groupby('Año')['Valor'].sum()
 print(ventas_por_anio)
 
-# Ventas por año
-ventas_por_año = df.groupby('Año')['Valor'].sum()
-print(ventas_por_año)
-
 # %%

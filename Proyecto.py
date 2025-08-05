40084b8e79f9d2b07db97ce61daa9c2473d4363a
DilanB <dbvlml@gmail.com>
2025-08-05 01:38:19 -0400

    Agregando datos de inicio al proyecto

--- Proyecto.py
+++ Proyecto.py
@@ -1,2 +1,16 @@
-print("hola")
-print("dilan")
+#%%
+import pandas as pd
+df = pd.read_csv('C:/Users/Dilan/Documents/Data analyst Contenido/Bamboo docs/Bamboo Tec Python/Ventas+Videojuegos.csv', sep=';')
+#print(df.head(10)) #ver las primeras 10 filas del DF
+
+# Eliminar comas y convertir a numérico
+df['Valor'] = df['Valor'].replace({',': '.'}, regex=True)
+
+# Convertir la columna a tipo numérico
+df['Valor'] = pd.to_numeric(df['Valor'])
+
+
+# Ventas por plataforma
+ventas_por_plataforma = df.groupby('Plataforma')['Valor'].sum()
+print(ventas_por_plataforma)
+# %%
